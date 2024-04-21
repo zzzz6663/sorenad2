0c9456cd4dc633952041e254d810689c13f4c428
naser <na3r.jafari@gmail.com>
2024-04-21 10:42:46 +0330

    403or2

--- resources/views/advertiser/site_script.blade.php
+++ resources/views/advertiser/site_script.blade.php
@@ -105,9 +105,9 @@
     <h2 class="nk-block-title fw-normal">اسکریت</h2>
 
     <div class="card">
-        <div class="card-inner"> 
+        <div class="card-inner">
             <div class="row">
-
+{{--  ddd  --}}
                 <div class="col-lg-6 mb-5">
                     <span class="pl-4">
                         کد تبلیغات پست ثابت (مناسب برای باتدا یا انتهای مطالب)
