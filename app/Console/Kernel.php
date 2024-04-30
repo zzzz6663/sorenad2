845fc6fffcfb45ae2b6c038fe3fdeb38c538f4ca
naser <na3r.jafari@gmail.com>
2024-04-30 18:34:45 +0330

    or1201

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('command:actionmanager')->everyMinute();
+        $schedule->command('command:actionmanager')->everySixHours();
     }
 
     /**
